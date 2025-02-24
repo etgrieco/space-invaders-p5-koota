752ee15147bbd12a9e6c0e713533366b8e2368e9
Elliott Grieco <elliott.grieco@gmail.com>
2025-02-24 17:44:30 -0500

    Add more to intro scene

--- src/scenes/introSimulation.ts
+++ src/scenes/introSimulation.ts
@@ -10,6 +10,15 @@ type IntroSimulationState = {
     posX: number;
     posY: number;
   };
+  pressToStartPos: {
+    posX: number;
+    posY: number;
+  };
+  pressToStartFill: {
+    /** Stored as value from 1 (visible) to 0 (invisible) */
+    alpha: number;
+    deltaVec: -1 | 1;
+  };
 };
 
 function createInitialIntroSimulationState(p: p5): IntroSimulationState {
@@ -20,6 +29,14 @@ function createInitialIntroSimulationState(p: p5): IntroSimulationState {
     introTextPos: {
       posX: 0,
       posY: p.height / 2,
+    },
+    pressToStartFill: {
+      alpha: 0,
+      deltaVec: 1,
+    },
+    pressToStartPos: {
+      posX: 0,
+      posY: p.height / 2 + p.textSize(),
     },
   };
 }
@@ -37,6 +54,17 @@ function introSimulationUpdater(
   if (!isHittingCeiling) {
     // TODO: Refactor into a generic position/velocity system
     state.introTextPos.posY -= CRAWL_INTRO_SPEED;
+    state.pressToStartPos.posY -= CRAWL_INTRO_SPEED;
+  } else {
+    // create a fun stuttery glow
+    if (p.frameCount % 5 === 0) {
+      if (state.pressToStartFill.alpha <= 0.5) {
+        state.pressToStartFill.deltaVec = 1;
+      } else if (state.pressToStartFill.alpha >= 1) {
+        state.pressToStartFill.deltaVec = -1;
+      }
+      state.pressToStartFill.alpha += state.pressToStartFill.deltaVec * 0.1;
+    }
   }
 
   return state;
@@ -45,11 +73,27 @@ function introSimulationUpdater(
 export function introSimulationFactory(
   p: p5
 ): TStateTickMachine<IntroSimulationState> {
+  const unmountSpecialKeyHandlers = new AbortController();
+  document.addEventListener(
+    "keydown",
+    function (e) {
+      if (e.code === "KeyS") {
+        window.alert("starting!");
+      }
+    },
+    {
+      signal: unmountSpecialKeyHandlers.signal,
+    }
+  );
+
   const stateMachine = {
     state: createInitialIntroSimulationState(p),
     tick() {
       this.state = introSimulationUpdater(p, this.state);
       return this.state;
+    },
+    cleanup() {
+      unmountSpecialKeyHandlers.abort();
     },
   };
   stateMachine.tick = stateMachine.tick.bind(stateMachine);
@@ -57,12 +101,25 @@ export function introSimulationFactory(
   return stateMachine;
 }
 export function drawIntro(p: p5, state: IntroSimulationState) {
+  // DRAW INTRO TEXT
   p.push();
-  p.fill("green");
+  p.textAlign("center", "bottom");
+  p.fill([178, 222, 39, 255]);
   p.text(
     "Welcome to Space Invaders!",
     state.introTextPos.posX,
     state.introTextPos.posY
+  );
+  p.pop();
+
+  // DRAW PRESS TO START
+  p.push();
+  p.textAlign("center", "bottom");
+  p.fill([178, 222, 39, state.pressToStartFill.alpha * 255]);
+  p.text(
+    "(press S to start)",
+    state.pressToStartPos.posX,
+    state.pressToStartPos.posY
   );
   p.pop();
 }
