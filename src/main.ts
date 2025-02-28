c1c14b43a2e9a035a740a9e7369c0aff14655ea8
Elliott Grieco <elliott.grieco@gmail.com>
2025-02-28 16:14:16 -0500

    Move copied game engine to three rendering strategy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import p5 from "p5";
+import * as THREE from "three";
 import { introSimulationFactory, drawIntro } from "./scenes/introSimulation";
-import { gameSimulationFactory } from "./scenes/gameSimulation";
+import {
+  gameSimulationFactory,
+  gameSimulationFactory_Three,
+} from "./scenes/gameSimulation";
 
 // Entrypoint code
 const rootEl = document.getElementById("p5-root");
@@ -8,6 +12,12 @@ if (!rootEl) {
   throw new Error("Cannot find element root #p5-root");
 }
 main(rootEl);
+// Entrypoint code
+const threeRootEl = document.getElementById("three-root");
+if (!threeRootEl) {
+  throw new Error("Cannot find element root #p5-root");
+}
+mainThree(threeRootEl);
 
 type AnimationScenes = {
   sceneId: string;
@@ -135,4 +145,61 @@ function myP5(p: p5) {
 
 function main(rootElement: HTMLElement) {
   new p5(myP5, rootElement);
+}
+
+function mainThree(mountingEl: HTMLElement) {
+  const WIDTH = 800;
+  const HEIGHT = 600;
+
+  const clock = new THREE.Clock();
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(WIDTH, HEIGHT);
+  mountingEl.appendChild(renderer.domElement);
+  renderer.setAnimationLoop(animate);
+
+  camera.position.z = 5;
+
+  const gameSim = gameSimulationFactory_Three(
+    {
+      camera,
+      scene,
+      webGlRenderer: renderer,
+      getDeltaTime: () => clock.getDelta() * 1000,
+    },
+    // TODO: for now, do nothing
+    () => {}
+  );
+
+  // basic way to wait for game to start, awaiting synchronization with intro
+  let gameStarted = false;
+  document.addEventListener("keydown", function (e) {
+    if (gameStarted) return;
+    if (e.code === "KeyS") {
+      gameStarted = true;
+      // disposal...
+      scene.remove(cube);
+    }
+  });
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  // setup animation loop
+  function animate() {
+    if (!gameStarted) {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    } else {
+      // add new cubes every second
+      // do logic stuff
+      gameSim.tick();
+    }
+    // then render
+    renderer.render(scene, camera);
+  }
 }
